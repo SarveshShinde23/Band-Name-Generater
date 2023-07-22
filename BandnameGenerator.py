3193884ef1912a79bc779ec3dc3997ed0526aa92
Sarvesh <126388862+SarveshShinde23@users.noreply.github.com>
2023-07-22 15:34:19 +0530

    Add files via upload

---
+++ BandnameGenerator.py
@@ -0,0 +1,9 @@
+#1.Create a greeting for your program
+print("Welcome to the band name Generater")
+#2.Ask the user for the city that they grew up in
+user=input("Enter your City name")
+#3.Ask the user for the name of pet
+use=input("Enter your pet name")
+#combine the name of their city and pet and show them their brand name.
+print("Here we go " +user+use)
+#5.Make sure the input cursor shows on line,see the example at:
